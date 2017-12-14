92aefa6fb149a1952d62d7ac2f4f7f378c4dff32
zhangchen <windyzj@163.com>
2017-12-15 00:12:46 +0800

    hello

---
+++ src/main/java/com/atguigu/boot2/AtguiguBoot2Application.java
@@ -0,0 +1,12 @@
+package com.atguigu.boot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtguiguBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtguiguBoot2Application.class, args);
+	}
+}
